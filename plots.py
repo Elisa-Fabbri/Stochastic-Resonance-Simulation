a8ca561edb359b1b121ad78638eced9afbd1654f
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-08-10 14:47:02 +0200

    Added function to plot F(T) using emission models in 'plots.py' file
    Implemented the 'Plot_F()' function in 'plots.py' to plot the behavior of F(T) using the two different models for emitted radiation.
    This function calculates and visualizes F(T) using the linear and black body models provided by the 'stochastic_resonance' module.
    The resulting plot is saved in the 'images' directory as 'F_plot.png'.

--- plots.py
+++ plots.py
@@ -30,8 +30,36 @@ def Plot_Emission_Models():
 
 	plt.show()
 
-Plot_Emission_Models()
 
+def Plot_F():
+
+	"""
+	This function plots F(T) using the two different models for the emitted radiation.
+	"""
+	T_start = 270
+	T_end = 300
+	num_points = 300
+
+	T = np.linspace(T_start, T_end, num_points)
+	F_linear_values = [stochastic_resonance.F_linear(Ti) for Ti in T]
+	F_black_body_values = [stochastic_resonance.F_black_body(Ti) for Ti in T]
+
+	f = plt.figure(figsize=(15, 10))
+	plt.plot(T, F_linear_values, label='Linear')
+	plt.plot(T, F_black_body_values, label = 'Black body')
+	plt.grid(True)
+	plt.xlabel('Temperature (K)')
+	plt.ylabel('F(T)')
+	#plt.legend()
+	plt.title('Comparison between F(T) using a linear and a black body model for beta')
+
+	save_file_path = os.path.join('images', 'F_plot.png')
+	plt.savefig(save_file_path)
+
+	plt.show()
+
+Plot_Emission_Models()
+Plot_F()
 
 
 
