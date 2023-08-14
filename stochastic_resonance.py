2d59b57988b6d0c5c9191ee1056b952a647e1a74
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-08-14 19:42:29 +0200

    Combined linear and black body emission models into a single function.
    Merged the functions linear_emitted_radiation and black_body_emitted_radiation into the function emitted_radiation.
    Also merged the functions F_linear and F_black_body into F.
    The emission model can now be selected using a new input parameter named 'model', which accepts values 'linear' or 'black_body'.
    If an invalid value is provided for the parameter, a ValueError is raised.

--- stochastic_resonance.py
+++ stochastic_resonance.py
@@ -1,21 +1,25 @@
 import numpy as np
 
-def linear_emitted_radiation(Temperature):
 
+def emitted_radiation(Temperature, model='linear'):
 	"""
-	This function computes the emitted radiation using a linear model.
+	This function computes the emitted radiation based on the selected model.
 	"""
-	A = -339.647*(number_of_seconds_in_a_year**3) #-339.647 W/m^2
-	B = 2.218*(number_of_seconds_in_a_year**3) #2.218 W/(m^2 K)
-	Emitted_radiation_linear = A + B*Temperature
-	return Emitted_radiation_linear
 
-def black_body_emitted_radiation(Temperature):
-	"""
-	This function computes the emitted radiation using the black body model
-	"""
-	Stefan_Boltzmann_constant = 5.67e-8*(number_of_seconds_in_a_year**3) #W/(m^2 K^4)
-	return Stefan_Boltzmann_constant*(Temperature**4)
+	number_of_seconds_in_a_year = 365 * 24 * 60 * 60
+
+	if model == 'linear':
+		A = -339.647 * (number_of_seconds_in_a_year ** 3)  # -339.647 W/m^2
+		B = 2.218 * (number_of_seconds_in_a_year ** 3)  # 2.218 W/(m^2 K)
+		Emitted_radiation = A + B * Temperature
+	elif model == 'black_body':
+		Stefan_Boltzmann_constant = 5.67e-8 * (number_of_seconds_in_a_year ** 3)  # W/(m^2 K^4)
+		Emitted_radiation = Stefan_Boltzmann_constant * (Temperature ** 4)
+	else:
+		raise ValueError("Invalid model selection. Choose 'linear' or 'black_body'.")
+
+	return Emitted_radiation
+
 
 def periodic_forcing(A, omega, time):
 	"""
@@ -23,18 +27,20 @@ def periodic_forcing(A, omega, time):
 	"""
 	return 1+A*np.cos(omega*time)
 
-def F_linear(Temperature, C, tau, T3, T1, T2, mu = 1):
-	"""
-	This function returns the value of F(T) given a certain value for the temperature using a linear model for the computation of beta.
-	"""
-	beta_linear = -((C/(tau*linear_emitted_radiation(T3)))*((T1*T2*T3) / ((T1 - T3)*(T2 - T3))))
-	F_linear = (linear_emitted_radiation(Temperature)/C)*((mu/(1 + beta_linear*(1-(Temperature/T1))*(1-(Temperature/T2))*(1-(Temperature/T3))))-1)
-	return F_linear
 
-def F_black_body(Temperature, C, tau, T3, T1, T2, mu = 1):
+def F(Temperature, C, tau, T3, T1, T2, model='linear', mu=1):
 	"""
-	This function returns the value of F(T) given a certain value for the temperature using the black body radiation model for the computation of beta.
+	This function returns the value of F(T) given a certain value for the temperature using a specified emission model for the computation of beta.
 	"""
-	beta_black_body = -((C/(tau*black_body_emitted_radiation(T3)))*((T1*T2*T3) / ((T1 - T3)*(T2 - T3))))
-	F_black_body = (black_body_emitted_radiation(Temperature)/C)*((mu/(1 + beta_black_body*(1-(Temperature/T1))*(1-(Temperature/T2))*(1-(Temperature/T3))))-1)
-	return F_black_body
+	if model == 'linear':
+		emitted_radiation_func = linear_emitted_radiation
+	elif model == 'black_body':
+		emitted_radiation_func = black_body_emitted_radiation
+	else:
+		raise ValueError("Invalid emission model selection. Choose 'linear' or 'black_body'.")
+
+	beta = -((C / (tau * emitted_radiation_func(T3))) * ((T1 * T2 * T3) / ((T1 - T3) * (T2 - T3))))
+	F_value = (emitted_radiation_func(Temperature) / C) * ((mu / (1 + beta * (1 - (Temperature / T1)) * (1 - (Temperature / T2)) * (1 - (Temperature / T3)))) - 1)
+
+	return F_value
+
