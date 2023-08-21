b7ef4e36d0aed9313f10c7f4ab4665bd42ef2166
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-08-21 13:38:36 +0200

    Added Simulation and Data Storage of Temperature and Time Dynamics to 'simulation.py' Module
    In this commit, I introduced simulations of temperature dynamics over time in the 'simulation.py' script.
    The simulations are performed using the 'simulate_ito' function from the 'stochastic_resonance.py' module.
    These simulations were conducted for two distinct levels of noise intensity, corresponding to different variance values.
    For each variance value, a series of 10 independent simulations were executed.
    
    The outcomes of these simulations are saved within the 'Temperature' array and the 'Time' array.
    The 'Temperature' array has dimensions of len(V), num_simulations, num_steps; while the 'Time' array has dimensions of len(V), num_steps.
    Here, the array 'V' encompasses the variance values utilized in the simulations — specifically, two values in this context.
    
    Subsequently, these arrays are stored within the newly created 'data' folder.
    
    The 'data' folder is not uploaded to the repository due to the large size of the files it contains.
    However, the 'data' folder is present in the local directory.
    This decision was made considering the substantial file sizes of the simulation outputs.

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 import numpy as np
 import stochastic_resonance
 
@@ -18,3 +19,33 @@ num_sec_in_a_year = 365.25*24*60*60
 
 C_years = C_j_per_m2_K*np.power(num_sec_in_a_year,2)
 omega = (2*np.pi)/period
+
+num_steps = 1000000
+num_simulations = 10
+
+V = np.linspace(0.01, 0.2, num = 2)
+sigma = np.sqrt(V)
+
+Time = np.zeros((len(V), num_steps))
+Temperature = np.zeros((len(V), num_simulations, num_steps))
+
+for i, v in enumerate(V):
+    time, simulated_temperature = stochastic_resonance.simulate_ito(T_start = T3, t_start = 0,
+					       			    noise_variance = v)
+    Temperature[i, :, :] = simulated_temperature
+    Time[i, :] = time
+    print('Simulation {0} of {1} done!'.format(i+1, len(V)))
+
+filename_temperature = 'simulated_temperature.npy'
+filename_time = 'simulated_time.npy'
+
+data_folder = 'data'
+os.makedirs(data_folder, exist_ok=True)
+
+save_file_path_temperature = os.path.join(data_folder, filename_temperature)
+save_file_path_time = os.path.join(data_folder, filename_time)
+
+np.save(save_file_path_temperature, Temperature)
+np.save(save_file_path_time, Time)
+
+print('Data saved successfully!')
