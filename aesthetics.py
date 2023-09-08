b4883f8f740c0601e9a9d04a4e3ee47fcb4440c1
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-09-08 15:15:09 +0200

    Enhanced 'aesthetics.py' with Code Formatting and Docstrings
    Updated the aesthetics.py file with code formatting improvements and added concise docstrings for the functions.

--- aesthetics.py
+++ aesthetics.py
@@ -2,9 +2,17 @@ import sys
 from contextlib import contextmanager
 
 def progress(iterator):
+    """
+    Show a progress bar while iterating over an iterable.
+
+    Parameters:
+    - iterator (iterable): The iterable to iterate over.
+
+    """
     current_iteration = 0
     total_iterations = len(iterator)
-    update_interval = int(total_iterations/100) if total_iterations>100 else 1
+    update_interval = int(total_iterations / 100) if total_iterations > 100 else 1
+
     for element in iterator:
         current_iteration += 1
         if current_iteration % update_interval == 0:
@@ -12,11 +20,15 @@ def progress(iterator):
             sys.stdout.write('\r[' + '-' * progress_percentage + ' ' * (100 - progress_percentage) + f'] {progress_percentage}%\r')
             sys.stdout.flush()
         yield element
+
     sys.stdout.write('\r[' + '-' * 100 + '] 100%\n')
     sys.stdout.flush()
 
 @contextmanager
 def green_text():
+    """
+    Context manager to temporarily print text in green color.
+    """
     print('\033[32m', end='')
     try:
         yield
@@ -25,6 +37,9 @@ def green_text():
 
 @contextmanager
 def orange_text():
+    """
+    Context manager to temporarily print text in orange color.
+    """
     print('\033[33m', end='')
     try:
         yield
@@ -33,6 +48,9 @@ def orange_text():
 
 @contextmanager
 def red_text():
+    """
+    Context manager to temporarily print text in red color.
+    """
     print('\033[91m', end='')
     try:
         yield
